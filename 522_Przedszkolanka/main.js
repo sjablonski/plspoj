7e70df37f81a87547de2e022a9141d2707958091
Szymon Jabłoński <sjablonski.it@gmail.com>
2020-11-21 15:08:44 +0100

    522 Przedszkolanka - Dodano rozwiązanie w JavaScript

---
+++ 522_Przedszkolanka/main.js
@@ -0,0 +1,16 @@
+function gcd(a, b) {
+  return a === 0 ? b : gcd(b % a, a);
+}
+
+let n = readline();
+n = isNaN(n) || n < 1 || n > 20 ? 0 : Number(n);
+
+for (let i = 0; i < n; i++) {
+  const numbers = readline().trim().split(" ");
+  const a = numbers[0];
+  const b = numbers[1];
+  10 <= a, b <= 30;
+  if (a >= 10 && a <= 30 && b >= 10 && b <= 30) {
+    console.log((a * b) / gcd(a, b));
+  }
+}
